40aa9e12690b7c03d50b52ce685d91b01cad8a2d
William Chu <willchu1733@gmail.com>
2024-06-05 11:27:01 +1200

    William-test

--- javascript/you.js
+++ javascript/you.js
@@ -1,4 +1,29 @@
-document.addEventListener("submit", (event) => {
-  event.preventDefault();
-  alert("Form submitted 🎉");
-});
+document.addEventListener('submit', (event) => {
+  event.preventDefault()
+  alert('Form submitted 🎉')
+})
+
+document.addEventListener('click', (event) => {
+  event.preventDefault()
+  const firstName = document.querySelector('#first_name')
+  const lastName = document.querySelector('#last_name')
+  const licenseNumber = document.querySelector('#license_number')
+  const disableButton = document.getElementById('submit-button')
+  if (
+    firstName.value.trim() == '' ||
+    lastName.value.trim() == '' ||
+    licenseNumber.value.trim() == ''
+  ) {
+    disableButton.disabled = true
+  } else {
+    disableButton.disabled = false
+  }
+
+  let todaysDate = new Date()
+  let formExpiryDate = document.getElementById('expiry_date')
+  let expiryDate = new Date(formExpiryDate.value)
+
+  if (expiryDate > todaysDate) {
+    console.log('Date: ', expiryDate, todaysDate)
+  }
+})
