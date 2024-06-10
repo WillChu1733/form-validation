a530ae07aaaa7f7dbecd8a0934ba7a97b3b17483
William Chu <willchu1733@gmail.com>
2024-06-10 20:32:50 +1200

    add queryselectorall for multiple fields

--- javascript/you.js
+++ javascript/you.js
@@ -1,27 +1,26 @@
-const formFields = document.querySelectorAll('.form-control')
+const formFields = document.querySelectorAll('.form-control') // returns a node list --> forEach
 const submitButton = document.getElementById('submit-button')
 
 // required fields = formFields, lastName, driversLicense, expiryDate, dob
+
+// removed submitButtontrue/false in loop as it is now set based on the value
+// of allValid after loop completes
 function validateForm() {
+  let allValid = true
   formFields.forEach((forms) => {
     if (forms.value.trim() == '') {
-      submitButton.disabled = true
-      console.log('submitbutton disabled')
-      return false
-    } else {
-      submitButton.disabled = false
-      console.log('submitbutton enabled')
-      return true
+      allValid = false
     }
   })
+  submitButton.disabled = !allValid // if allValid is true, button.disable = false
+  return allValid
 }
 
 document.addEventListener('submit', (event) => {
-  console.log('helloio')
   if (!validateForm()) {
     event.preventDefault()
 
-    alert('unable to submit') // need to add validateForm here?? to check if whitespace again
+    alert('unable to submit')
   } else {
     alert('Form submitted 🎉')
   }
