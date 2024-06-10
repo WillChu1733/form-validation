2a58c1fe7a2b64e18af801956eb4ad3110b8505e
William Chu <willchu1733@gmail.com>
2024-06-10 22:47:18 +1200

    get current date and get expiry date (user input) ; git push

--- javascript/you.js
+++ javascript/you.js
@@ -1,6 +1,15 @@
 const formFields = document.querySelectorAll('.form-control') // returns a node list --> forEach
 const submitButton = document.getElementById('submit-button')
 
+let currentDate = new Date()
+
+// get the date of user input
+let expiryDate = document.getElementById('expiry_date')
+expiryDate.addEventListener('input', () => {
+  let date = new Date(expiryDate.value)
+  console.log(date)
+})
+
 // required fields = formFields, lastName, driversLicense, expiryDate, dob
 
 // removed submitButtontrue/false in loop as it is now set based on the value
@@ -13,16 +22,24 @@ function validateForm() {
     }
   })
   submitButton.disabled = !allValid // if allValid is true, button.disable = false
+
   return allValid
 }
 
 document.addEventListener('submit', (event) => {
-  if (!validateForm()) {
+  if (!validateForm() || expiryDate > currentDate) {
     event.preventDefault()
 
-    alert('unable to submit')
+    alert('expiry date cannot be in the past')
+    console.log('current date:', currentDate)
+    console.log('expiry date:', expiryDate)
+    console.log('ValidateForm:', allValid)
   } else {
     alert('Form submitted 🎉')
+    event.preventDefault()
+    console.log('current date:', currentDate)
+    console.log('expiry date:', expiryDate)
+    console.log('ValidateForm:', allValid)
   }
 })
 
@@ -35,3 +52,8 @@ document.addEventListener('blur', (event) => {
   event.preventDefault()
   validateForm()
 })
+
+// expiry date must be greater than todays date
+// expiryDate > currentDate
+// format todayDate in dd/mm/yyyy
+// expiryDate = user input
