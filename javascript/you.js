0c2820326744e1d788fe8492c028a0e72c628e3d
William Chu <willchu1733@gmail.com>
2024-06-10 13:11:31 +1200

    blank test

--- javascript/you.js
+++ javascript/you.js
@@ -1,29 +1,5 @@
-const firstName = document.querySelector('#first_name')
-const submitButton = document.getElementById('submit-button')
-
-function validateForm() {
-  if (firstName.value.trim() == '') {
-    submitButton.disabled = true
-
-    return false
-  } else {
-    submitButton.disabled = false
-    return true
-  }
-}
-
 document.addEventListener('submit', (event) => {
-  if (!validateForm()) {
-    event.preventDefault()
-  } else {
-    alert('Form submitted 🎉')
-  }
-})
-
-firstName.addEventListener('input', validateForm)
-
-// loss of Focus validation
-document.addEventListener('blur', (event) => {
   event.preventDefault()
-  validateForm()
+
+  alert('Form submitted 🎉')
 })
