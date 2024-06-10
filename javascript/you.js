e671749f88efe085a7180c317251709a048248d8
William Chu <willchu1733@gmail.com>
2024-06-10 13:00:37 +1200

    Create MVP with validation on single form

--- javascript/you.js
+++ javascript/you.js
@@ -1,29 +1,29 @@
+const firstName = document.querySelector('#first_name')
+const submitButton = document.getElementById('submit-button')
+
+function validateForm() {
+  if (firstName.value.trim() == '') {
+    submitButton.disabled = true
+
+    return false
+  } else {
+    submitButton.disabled = false
+    return true
+  }
+}
+
 document.addEventListener('submit', (event) => {
-  event.preventDefault()
-  alert('Form submitted 🎉')
+  if (!validateForm()) {
+    event.preventDefault()
+  } else {
+    alert('Form submitted 🎉')
+  }
 })
 
-document.addEventListener('click', (event) => {
+firstName.addEventListener('input', validateForm)
+
+// loss of Focus validation
+document.addEventListener('blur', (event) => {
   event.preventDefault()
-  const firstName = document.querySelector('#first_name')
-  const lastName = document.querySelector('#last_name')
-  const licenseNumber = document.querySelector('#license_number')
-  const disableButton = document.getElementById('submit-button')
-  if (
-    firstName.value.trim() == '' ||
-    lastName.value.trim() == '' ||
-    licenseNumber.value.trim() == ''
-  ) {
-    disableButton.disabled = true
-  } else {
-    disableButton.disabled = false
-  }
-
-  let todaysDate = new Date()
-  let formExpiryDate = document.getElementById('expiry_date')
-  let expiryDate = new Date(formExpiryDate.value)
-
-  if (expiryDate > todaysDate) {
-    console.log('Date: ', expiryDate, todaysDate)
-  }
+  validateForm()
 })
